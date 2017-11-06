3927d1902101013bcfb16d46c742315b291715de
giovanniani <giovanni_ani@hotmail.com>
2017-11-06 15:42:10 -0600

    lucene

---
+++ RIT_Proyecto2/src/com/lucene/fuzzy/TextFileFilter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lucene.fuzzy;
+import java.io.File;
+import java.io.FileFilter;
+public class TextFileFilter implements FileFilter {
+ @Override
+ public boolean accept(File pathname) {
+ return pathname.getName().toLowerCase().endsWith(".txt");
+ }
+}
