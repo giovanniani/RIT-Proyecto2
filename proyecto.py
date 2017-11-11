09af0ec25cefc34683bbc3384ae768b8cbd758a7
giovanniani <giovanni_ani@hotmail.com>
2017-11-11 14:20:04 -0600

    diccionario creado

--- proyecto.py
+++ proyecto.py
@@ -18,41 +18,38 @@ def getPaths(pPath):    #recibe la ruta del fichero original y devuelve todas la
         for fil in onlyFiles:
             if fil.endswith(".htm"):
                 allPaths.append(x[0] + "/" + fil)
-    print(allPaths)
     return allPaths
     
-def examineFiles(pFilesList,pPath):
+def examineFile(pFile):
     ref = set([])        #campo donde se guardan las referencias
     text = set([])       #campo donde se guardan los terminos
     texto = ""      #almacena todo el texto normal de la página, incluidas las referencias
-    for file in pFilesList:
-        print(file)
-        f = open(file, "r")
-        document = f.read()     
-        document = document.decode('utf8')
-        document = document.lower()
-        match = re.findall(u'<p>(.+)<\/p>',document)    #encuentra todo lo que esta dentro de <p>...</p>
-        f.close()
-        if match:
-            for mActual in match:
-                                #viene separacion de texto de referencia y texto normal
-                matchP = re.findall(u'<a [\)\(#\w\ =\"\/%a-zA-ZáéíóúüÁÉÍÓÚÜñÑ,\n\-\_]+>([\d\(\ =\"\/%a-zA-ZáéíóúüÁÉÍÓÚÜñÑ,]+)<\/a>|(?!<)([\)\(#\ =%a-zA-ZáéíóúüÁÉÍÓÚÜñÑ\,\n\-\_\"\d]+)(?!>)',mActual)
-                if matchP:
-                    for match2 in matchP:
-                        if len(match2[0]) > 0:
-                            ref.add(match2[0])
-                            texto = texto + " " + match2[0]
-                        else:
-                            pass
-                        if len(match2[1]) > 0:
-                            texto = texto + " " + match2[1]
-                        else:
-                            pass
-        match = re.findall(u'([a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]+)',texto)       #luego de obtenido el texto normal de la pagina se viene la depuración del mismo
-        if match:
-            for word in match:
-                text.add(word)
-        return text
+    f = open(pFile, "r")
+    document = f.read()     
+    document = document.decode('utf8')
+    document = document.lower()
+    match = re.findall(u'<p>(.+)<\/p>',document)    #encuentra todo lo que esta dentro de <p>...</p>
+    f.close()
+    if match:
+        for mActual in match:
+                            #viene separacion de texto de referencia y texto normal
+            matchP = re.findall(u'<a [\)\(#\w\ =\"\/%a-zA-ZáéíóúüÁÉÍÓÚÜñÑ,\n\-\_]+>([\d\(\ =\"\/%a-zA-ZáéíóúüÁÉÍÓÚÜñÑ,]+)<\/a>|(?!<)([\)\(#\ =%a-zA-ZáéíóúüÁÉÍÓÚÜñÑ\,\n\-\_\"\d]+)(?!>)',mActual)
+            if matchP:
+                for match2 in matchP:
+                    if len(match2[0]) > 0:
+                        ref.add(match2[0])
+                        texto = texto + " " + match2[0]
+                    else:
+                        pass
+                    if len(match2[1]) > 0:
+                        texto = texto + " " + match2[1]
+                    else:
+                        pass
+    match = re.findall(u'([a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]+)',texto)       #luego de obtenido el texto normal de la pagina se viene la depuración del mismo
+    if match:
+        for word in match:
+            text.add(word)
+    return text
         
 def removeStopWords(pPath,pList):
     stopWords = []
@@ -63,7 +60,6 @@ def removeStopWords(pPath,pList):
         stopWords.append(line.rstrip("\n\r"))
     for data in stopWords:
         if data in pList:
-            print(data)
             pList.remove(data)
         else:
             pass
@@ -71,16 +67,37 @@ def removeStopWords(pPath,pList):
     return pList
 
 def stemmer(pList):
+    stemmer = snowballstemmer.stemmer('spanish')
     stemmedWords = set([])
     for word in pList:
         stemmed = stemmer.stemWord(word)
-        dictionary.add(word)
-    
+        stemmedWords.add(word)
+    return stemmedWords
 
+def saveDictionary(pFilePath,pList,pDicPath,pIndex):
+    name = pFilePath.split("/")
+    name = name[len(name)-1].split(".")[0]
+    name = "D"+ str(pIndex) + "-" + name
+    f = open(pDicPath + "/" + name + "-dicionary.txt", "w+")
+    f.write(name + "\n")
+    for i in range(0,len(pList)-2,1):
+        data = list(pList)[i]
+        data = data.encode('utf-8')
+        f.write(data + ", ")
+    data = list(pList)[len(pList)-1]
+    data = data.encode('utf-8')
+    f.write(data)
+    f.close()
+        
 def main(pPath):
-    sWPath = input("Ingrese la ubicación del archivo de stopwords: \n")
-    print("entrando")
+    sWPath = input(u"Ingrese la ubicación del archivo de stopwords: \n")
+    dicPath = input(u"Ingrese la ubicación donde se guardará los diccionarios de palabras: \n")
     files = getPaths(pPath)
-    text = examineFiles(files,pPath)
-    textSW = removeStopWords(sWPath,text)
+    for fileDir in files:
+        text = examineFile(fileDir)
+        textSW = removeStopWords(sWPath,text)
+        stemmed = stemmer(textSW)
+        saveDictionary(fileDir,stemmed,dicPath,files.index(fileDir))
+        
+
     
